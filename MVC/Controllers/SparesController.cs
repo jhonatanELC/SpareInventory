bb6afcd778130292348231aa7807048fdce1b84e
jhonatanELC <larico1599@outlook.com>
2024-05-14 13:52:49 -0500

    CreateSpare View ended

--- MVC/Controllers/SparesController.cs
+++ MVC/Controllers/SparesController.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Core.Domain.Entities;
 using Infrastructure;
 using Core.Dtos.Filters;
 using Core.Dtos.SpareDto;
 using Core.Contracts.Service.SpareService;
-using System.Globalization;
 using Core.Enums;
+using Core.Contracts.Service.Brand;
+using Core.Dtos.BrandDto;
+using MVC.Models;
+using Core.Dtos.SpareBrandDto;
+using Core.Contracts.Service.SpareBrandService;
+using Core.Dtos.PriceDto;
 
 namespace MVC.Controllers
 {
@@ -17,28 +20,33 @@ namespace MVC.Controllers
       private readonly ISpareAddService _spareAddService;
       private readonly ISpareGetService _spareGetService;
       private readonly ISpareDeleteService _spareDeleteService;
-      private readonly SpareInventoryDbContext _context;
-      public SparesController(ISpareAddService spareAddService, ISpareGetService spareGetService, ISpareDeleteService spareDeleteService, SpareInventoryDbContext context)
+      private readonly IBrandGetService _brandGetService;
+      private readonly IBrandAddService _brandAddService;
+      private readonly ISpareBrandAddService _spareBrandAddService;
+
+      public SparesController(ISpareAddService spareAddService, ISpareGetService spareGetService, ISpareDeleteService spareDeleteService, IBrandGetService brandGetService, IBrandAddService brandAddService, ISpareBrandAddService spareBrandAddService)
       {
          _spareAddService = spareAddService;
          _spareGetService = spareGetService;
          _spareDeleteService = spareDeleteService;
-         _context = context;
+         _brandGetService = brandGetService;
+         _brandAddService = brandAddService;
+         _spareBrandAddService = spareBrandAddService;
       }
 
       [Route("[action]")]
       [Route("/")]
-      public async Task<IActionResult> Index(string? currentsearchValue, string? currentSearchBy= "searchrBySku", string? currentFilterByValue = "Ninguno")
+      public async Task<IActionResult> Index(string? currentsearchValue, string? currentSearchBy = "searchrBySku", string? currentFilterByValue = "Ninguno")
       {
 
          SpareFilter filter = new();
          filter.filterByGroup = currentFilterByValue;
 
-         if(currentSearchBy == "searchrBySku")
+         if (currentSearchBy == "searchrBySku")
          {
             filter.searchrBySku = currentsearchValue;
          }
-         else if(currentSearchBy == "filterByOemCode")
+         else if (currentSearchBy == "filterByOemCode")
          {
             filter.filterByOemCode = currentsearchValue;
          }
@@ -65,7 +73,7 @@ namespace MVC.Controllers
          };
 
          ViewData["groups"] = groups;
-         
+
 
          return View(spares);
       }
@@ -90,30 +98,56 @@ namespace MVC.Controllers
       //   return View(spare);
       //}
 
-      // GET: Spares/Create
-      //public IActionResult Create()
-      //{
-      //   ViewData["VehicleId"] = new SelectList(_context.Vehicles, "VehicleId", "Brand");
-      //   return View();
-      //}
+      //GET: Spares/Create
+      [Route("[action]")]
+      public async Task<IActionResult> Create()
+      {
+         IReadOnlyList<BrandToReturn> brands = await _brandGetService.GetBrands();
+         IReadOnlyList<SpareToReturn> spares = await _spareGetService.GetSpares();
 
+         // Creates a selectListItems of brands
+         ViewData["Brands"] = brands.Select(b => new SelectListItem() { Text = b.Name, Value = b.BrandId.ToString() });
+
+         // Creates a selectListItems of units
+         string[] units = { "Unidad", "Par" };
+         ViewData["Units"] = units.Select(u => new SelectListItem() { Text = u, Value = u });
+
+         // Creates a selectListItems of groups
+         ViewData["Groups"] = spares.DistinctBy(s => s.Group).Select(s => new SelectListItem() { Text = s.Group.ToString(), Value = s.Group.ToString() });
+
+         return View();
+      }
+
+      // TODO 4: Implement Service to Create a Spare with brands and Price
       // POST: Spares/Create
       // To protect from overposting attacks, enable the specific properties you want to bind to.
       // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-      //[HttpPost]
-      //[ValidateAntiForgeryToken]
-      //public async Task<IActionResult> Create([Bind("SpareId,Sku,Description,Comments,OemCode,Group,VehicleId")] Spare spare)
-      //{
-      //   if (ModelState.IsValid)
-      //   {
-      //      spare.SpareId = Guid.NewGuid();
-      //      _context.Add(spare);
-      //      await _context.SaveChangesAsync();
-      //      return RedirectToAction(nameof(Index));
-      //   }
-      //   ViewData["VehicleId"] = new SelectList(_context.Vehicles, "VehicleId", "Brand", spare.VehicleId);
-      //   return View(spare);
-      //}
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public async Task<IActionResult> Create( SpareToAddVm spareVm)
+      {
+         if (ModelState.IsValid)
+         {
+            // Adding a Spare
+            SpareToAdd spare = spareVm.GetSpareToAdd();
+
+            var spareCreated = await _spareAddService.AddSpare(spare);
+      
+
+            // Adding Brand to Spare
+            SpareBrandToAdd sparebrand = spareVm.GetSpareBrandToAdd();
+            sparebrand.SpareId = spareCreated.SpareId;
+            sparebrand.BrandId = spareVm.BrandId;
+
+            PriceToAdd price = spareVm.GetPriceToAdd();
+
+            await _spareBrandAddService.AddBrandToSpare(sparebrand, price);
+
+            return RedirectToAction(nameof(Index));
+         }
+
+         return View();
+      }
 
       // GET: Spares/Edit/5
       //public async Task<IActionResult> Edit(Guid? id)
@@ -168,24 +202,25 @@ namespace MVC.Controllers
       //   return View(spare);
       //}
 
-      //// GET: Spares/Delete/5
-      //public async Task<IActionResult> Delete(Guid? id)
-      //{
-      //   if (id == null)
-      //   {
-      //      return NotFound();
-      //   }
+      // GET: Spares/Delete/5
+      [Route("[action]/{spareId}")]
+      public async Task<IActionResult> Delete(Guid? spareId)
+      {
+         if (spareId == null)
+         {
+            return NotFound();
+         }
 
-      //   var spare = await _context.Spares
-      //       .Include(s => s.Vehicle)
-      //       .FirstOrDefaultAsync(m => m.SpareId == id);
-      //   if (spare == null)
-      //   {
-      //      return NotFound();
-      //   }
-
-      //   return View(spare);
-      //}
+         //var spare = await _context.Spares
+         //    .Include(s => s.Vehicle)
+         //    .FirstOrDefaultAsync(m => m.SpareId == id);
+         //if (spare == null)
+         //{
+         //   return NotFound();
+         //}
+         return View();
+         //return View(spare);
+      }
 
       //// POST: Spares/Delete/5
       //[HttpPost, ActionName("Delete")]
---
+++ MVC/Models/SpareToAddVm.cs
@@ -0,0 +1,76 @@
+﻿using Core.Dtos.PriceDto;
+using Core.Dtos.SpareBrandDto;
+using Core.Dtos.SpareDto;
+using Core.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Models
+{
+   [Bind]
+   public class SpareToAddVm
+   {
+      // Spare
+      public string Description { get; set; }
+      public string? Comments { get; set; }
+      public string? OemCode { get; set; }
+      public string Sku { get; set; }
+      public Group Group { get; set; }
+      //public Guid? VehicleId { get; set; }
+
+      // Price
+      public Currency Currency { get; set; }
+      public decimal UnitPrice { get; set; }
+      public int Igv { get; set; }
+      public int ProfitMargin { get; set; }
+      public decimal SellPrice { get; set; }
+
+      // Brand
+      public Guid BrandId { get; set; }
+
+      // SpareBrand
+      public string? CodeByBrand { get; set; }
+      public string Unit { get; set; }
+
+
+
+      public SpareToAdd GetSpareToAdd()
+      {
+         SpareToAdd spare = new SpareToAdd();
+
+         spare.Description = Description;
+         spare.Comments = Comments;
+         spare.OemCode = OemCode;
+         spare.Sku = Sku;
+         spare.Group = Group;
+         //spare.VehicleId = VehicleId;
+
+         return spare;
+      }
+
+      public SpareBrandToAdd GetSpareBrandToAdd()
+      {
+         SpareBrandToAdd spareBrand = new SpareBrandToAdd();
+
+         spareBrand.CodeByBrand = CodeByBrand;
+         spareBrand.Unit = Unit;
+
+         return spareBrand;
+      }
+
+      public PriceToAdd GetPriceToAdd()
+      {
+         PriceToAdd price = new PriceToAdd();
+
+         price.UnitPrice = UnitPrice;
+         price.SellPrice = SellPrice;
+         price.ProfitMargin = ProfitMargin;
+         price.Currency = Currency;
+         price.Igv = Igv;
+
+         return price;
+      }
+
+   }
+
+
+}
